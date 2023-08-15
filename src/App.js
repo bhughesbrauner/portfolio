caacc822528c1f8417a3ae688c6fe7836b862604
Brooke Hughes-Brauner <b.hughes.brauner@gmail.com>
2023-08-15 00:17:58 -0700

    Created Not working Sidebar, and basic routing

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.scss';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+function App() {
+  return (
+    // <Routes>
+    //   <Route path="/" element={<Layout />} />
+    //   <div>
+    //     Hello
+    //   </div>
+    // </Routes>
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+import Sidebar from '../Sidebar';
+
+const Layout = () => {
+    return <>
+        <div>
+            Hello
+        </div>
+        <Sidebar />
+    </>
+}
+
+export default Layout;
