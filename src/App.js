197ca255660e872aa3c305bbc993ceeffe08cbe7
Brooke Hughes-Brauner <b.hughes.brauner@gmail.com>
2023-08-15 14:33:04 -0700

    Redid structure so that src/node/public etc. are all at root

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import './App.scss';
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    // <Routes>
-    //   <Route path="/" element={<Layout />} />
-    //   <div>
-    //     Hello
-    //   </div>
-    // </Routes>
-    <div>
-      Hello
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
